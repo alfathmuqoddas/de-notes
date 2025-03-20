ec68f0935b98d09b4864fa3fa9c8c57a6e03e77e
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-03-20 13:33:24 +0700

    update: integrate zustand to store auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { Button } from "./components/ui/button";
-import { useState, useEffect } from "react";
-import { ModeToggle } from "./components/mode-toggle";
-import { onAuthStateChanged, User } from "firebase/auth";
-import { auth } from "./lib/firebase";
 import { SignInButton } from "./components/Buttons/SignInButton";
 
 const App = () => {
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    try {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          console.log("User is signed in");
-          setUser(user);
-        } else {
-          console.log("User is signed out");
-          setUser(null);
-        }
-      });
-    } catch (error) {
-      console.error(error);
-      setUser(null);
-    }
-  }, []);
-
   return (
     <div className="flex flex-col items-center justify-center h-screen text-black">
       <SignInButton />
-      <pre>
-        {user
-          ? JSON.stringify(user.displayName, null, 2)
-          : "User not logged in"}
-      </pre>
     </div>
   );
 };
--- src/components/Buttons/SignInButton.tsx
+++ src/components/Buttons/SignInButton.tsx
@@ -1,37 +1,32 @@
 import { Button } from "../ui/button";
 import { auth } from "../../lib/firebase";
-import { useState, useEffect } from "react";
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-  onAuthStateChanged,
-  User,
-} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import useAuthStore from "@/store/useAuthStore";
 
 export function SignInButton() {
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState<User | null>(null);
+  const { user, setUser } = useAuthStore();
 
   const handleSignIn = async () => {
-    setLoading(true);
     try {
       const result = await signInWithPopup(auth, new GoogleAuthProvider());
       if (result.user) {
-        console.log("User signed in", result.user);
+        console.log("User signed in");
+        setUser(result.user);
       } else {
-        console.log("No user");
+        console.log("User not signed in");
+        setUser(null);
       }
     } catch (error) {
       console.error(error);
+      setUser(null);
     }
-    setLoading(false);
   };
 
   const handleSignOut = async () => {
     try {
       if (window.confirm("Are you sure you want to sign out?")) {
         await signOut(auth);
+        setUser(null);
       } else {
         return;
       }
@@ -40,28 +35,15 @@ export function SignInButton() {
     }
   };
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
-        console.log("User is signed in");
-      } else {
-        setUser(null);
-        console.log("User is signed out");
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
   return (
     <div>
       {user ? (
-        <Button variant={"destructive"} size={"lg"} onClick={handleSignOut}>
-          Sign Out
-        </Button>
+        <>
+          <Button variant={"destructive"} size={"lg"} onClick={handleSignOut}>
+            Sign Out
+          </Button>
+          <p>{user?.displayName}</p>
+        </>
       ) : (
         <Button size={"lg"} onClick={handleSignIn}>
           Sign In
